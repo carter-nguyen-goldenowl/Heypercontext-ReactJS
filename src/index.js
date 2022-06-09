ba3df92b6f712262c83678fc9a5b617dd8471488
carter-nguyen-goldenowl <carter.nguyen.goldenowl@gmail.com>
2022-06-09 09:54:04 +0700

    implement redux-persist

--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
+let persistor = persistStore(store);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import * as authSlice from "../components/frontend/auth/authSlice";
 import * as taskSlice from "../components/frontend/Task/taskSlice";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const reducers = combineReducers({
+  auth: authSlice.reducer,
+  task: taskSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    auth: authSlice.reducer,
-    task: taskSlice.reducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
